425afef577398654788926481af0b752d73276b6
BinhNguyen1234 <88345612+BinhNguyen1234@users.noreply.github.com>
2023-04-22 17:23:42 +0700

    init app:

---
+++ Model/Product.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace dotnet_vite_vuejs.Model
+{
+	[Table("SanPham")]
+	public class Product
+	{
+		public Product()
+		{
+		
+		}
+		[Key]
+		public int ProductId { get; set; }
+		[Required]
+		[StringLength(50)]
+		public string Name { get; set; }
+		[Required]
+		public string Provide { get; set; }
+    }
+	public class ProductContext : DbContext
+	{
+		public DbSet<Product> product { set; get; }
+		private string connectionString = @"hostname=cmsblog123.mysql.database.azure.comusername=binhnguyenpassword=M@roen4eessl-mode=require";
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+			optionsBuilder.UseSqlServer(connectionString);
+        }
+    }
+
+}
+
--- Program.cs
+++ Program.cs
@@ -1,26 +1,46 @@
+using dotnet_vite_vuejs.Model;
+using System;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Hosting;
-
+using Microsoft.EntityFrameworkCore;    
 namespace dotnet_vite_vuejs
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
+            Console.WriteLine("app start");
             var builder = createWebBuilder(args);
             var app = builder.Build();
             var startup = new Startup(builder, builder.Environment);
             startup.ConfigureServices(builder.Services);
             startup.Configure(app, app.Environment);
             app.Run();
+            await CreateDatabase();
+            Console.ReadLine();
           
         }
+        public static async Task CreateDatabase()
+        {
+            using (var dbcontext = new ProductContext())
+            {
+                String databasename = dbcontext.Database.GetDbConnection().Database;// mydata
+
+                Console.WriteLine("Tạo " + databasename);
+
+                bool result = await dbcontext.Database.EnsureCreatedAsync();
+                string resultstring = result ? "tạo  thành  công" : "đã có trước đó";
+                Console.WriteLine($"CSDL {databasename} : {resultstring}");
+            }
+        } 
         private static WebApplicationBuilder createWebBuilder(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
             return builder;
         }
     }
+
 }
