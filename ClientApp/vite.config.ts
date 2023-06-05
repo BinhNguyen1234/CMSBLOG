c7c2d6dd2649c25cf82c100a3a9f175b2db5f1d5
DESKTOP-20BACJI\Admin <binh.ngv.dev@gmail.com>
2023-06-06 00:59:22 +0700

    config for proxy to BE from FE

--- ClientApp/vite.config.ts
+++ ClientApp/vite.config.ts
@@ -1,12 +1,17 @@
 import { defineConfig, loadEnv } from 'vite'
 import basicSsl from '@vitejs/plugin-basic-ssl'
 import vue from '@vitejs/plugin-vue'
-let a = process.env;
-// https://vitejs.dev/config/
+
+const PORT_FOR_PROXY = process.env.ASPNETCORE_HTTPS_PORT
 export default defineConfig({
     plugins: [vue(), basicSsl()],
     server: {
-        https: true
+        https: true,
+        proxy: {
+            '/api': {
+                target: `https://localhost:${PORT_FOR_PROXY}`,
+                secure: false
+            }
+        }
     }
 })
-console.debug('[vite] connecting...', a);
