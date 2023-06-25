e80baab2c5c70f73d28ccf3ebd71b63507fdcd84
TITANCORP\binh.nguyenv <binh.nguyenv@dmchosting.ca>
2023-06-25 16:02:05 +0700

    .

--- ClientApp/src/App.vue
+++ ClientApp/src/App.vue
@@ -1,16 +1,19 @@
 <script setup lang="ts">
 import HelloWorld from './components/HelloWorld.vue'
+import vitelogo from "@assets/vite.svg"
+import vuelog from "@assets/vue.svg"
 </script>
 
 <template>
   <div>
     <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
+      <img :src="vitelogo" class="logo" alt="Vite logo" />
     </a>
     <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
+      <img :src="vuelog" class="logo vue" alt="Vue logo" />
     </a>
   </div>
+  <div id="app2"></div>
   <HelloWorld msg="Vite + Vue" />
 </template>
 
--- ClientApp/vite.config.ts
+++ ClientApp/vite.config.ts
@@ -1,17 +1,21 @@
-import { defineConfig, loadEnv } from 'vite'
-import basicSsl from '@vitejs/plugin-basic-ssl'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import basicSsl from "@vitejs/plugin-basic-ssl";
+import vue from "@vitejs/plugin-vue";
 
-const PORT_FOR_PROXY = process.env.ASPNETCORE_HTTPS_PORT
+const PORT_FOR_PROXY = process.env.ASPNETCORE_HTTPS_PORT;
 export default defineConfig({
-    plugins: [vue(), basicSsl()],
-    server: {
-        https: true,
-        proxy: {
-            '/api': {
-                target: `https://localhost:${PORT_FOR_PROXY}`,
-                secure: false
-            }
-        }
-    }
-})
+  plugins: [vue(), basicSsl()],
+  server: {
+    https: true,
+    proxy: {
+      "/api": {
+        target: `https://localhost:${PORT_FOR_PROXY}`,
+        secure: false,
+      },
+    },
+  },
+  resolve: {
+    extensions: [".vue",".svg"],
+    alias: [{ find: "@assets", replacement: "/src/assets" }],
+  },
+});
