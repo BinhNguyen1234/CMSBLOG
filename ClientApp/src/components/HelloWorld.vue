f09f5a8337e907ea12811c24c436908c925ab75e
DESKTOP-20BACJI\Admin <binh.ngv.dev@gmail.com>
2023-06-25 23:44:43 +0700

    init test branch

--- ClientApp/src/components/HelloWorld.vue
+++ ClientApp/src/components/HelloWorld.vue
@@ -1,16 +1,29 @@
 <script setup lang="ts">
-import { ref } from 'vue'
+import { ref,reactive,onUpdated, nextTick } from 'vue'
 
 defineProps<{ msg: string }>()
+let a = 'color:red;'
+let count = ref(0)
+const count2 = reactive ({
+  value: 0
+})
+let { value } = count2
+function CountC(){
+  
+  count2.value +=1;
+  nextTick()
+}
+onUpdated(()=>{
+  console.log(3123213)
+})
 
-const count = ref(0)
 </script>
 
 <template>
   <h1>{{ msg }}</h1>
 
   <div class="card">
-    <button type="button" @click="count++">count is {{ count }}</button>
+    <button :disabled="count >= 10" type="button" @click="CountC">count is {{ value }}</button>
     <p>
       Edit
       <code>components/HelloWorld.vue</code> to test HMR
@@ -23,15 +36,15 @@ const count = ref(0)
       >create-vue</a
     >, the official Vue + Vite starter
   </p>
-  <p>
+  <p>o
     Install
     <a href="https://github.com/vuejs/language-tools" target="_blank">Volar</a>
     in your IDE for a better DX
   </p>
-  <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
+  <p v-if="count % 2 == 0" v-bind:style="a" class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
 </template>
 
-<style scoped>
+<style scoped> 
 .read-the-docs {
   color: #888;
 }
